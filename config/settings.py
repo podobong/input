22118664d39108a2d5edf9fac72776e0d49c0f67
podobongbong <kim11560@snu.ac.kr>
2020-07-01 17:52:59 +0900

    Initial commit

---
+++ config/settings.py
@@ -0,0 +1,100 @@
+import os
+import json
+
+from config.keys import db
+
+
+# Absolute Path of Root Directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Django Project Secret Key
+with open(os.path.join(BASE_DIR, 'conf/keys/secret_key.json')) as f:
+    SECRET_KEY = json.loads(f.read())['SECRET_KEY']
+
+# Debug Mode
+DEBUG = True
+
+# Allowed Hosts
+ALLOWED_HOSTS = ['localhost']
+
+# Installed Apps
+INSTALLED_APPS = [
+    # Default Installed Apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # 3rd Party Apps
+    'rest_framework',
+
+    # Custom Apps
+]
+
+# Language / Timezone
+LANGUAGE_CODE = 'ko-kr'
+
+TIME_ZONE = 'Asia/Seoul'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Static Files / Media Files
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+# Databases
+DATABASES = db.DATABASES
+
+# URL
+ROOT_URLCONF = 'conf.urls'
+
+# Templates
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Middleware
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Authentication
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
+# WSGI
+WSGI_APPLICATION = 'conf.wsgi.application'
+
