3f3afbd2aa369e59ff9ed482ddb964006db9a217
podobongbong <kim11560@snu.ac.kr>
2020-07-05 01:28:05 +0900

    Merged views

--- univ/urls.py
+++ univ/urls.py
@@ -5,5 +5,4 @@ from univ import views
 
 urlpatterns = [
     path('', views.UnivList.as_view()),
-    path('device/', views.DeviceInfo.as_view()),
 ]
--- univ/views.py
+++ univ/views.py
@@ -1,6 +1,5 @@
 from django.db.models import Q
 from django.db import IntegrityError
-from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -9,13 +8,19 @@ from univ.models import Univ, Major, Device
 from univ.serializers import UnivSerializer
 
 
-class UnivList(generics.ListAPIView):
-    queryset = Univ.objects.all()
-    serializer_class = UnivSerializer
-
-
-class DeviceInfo(APIView):
+class UnivList(APIView):
     def get(self, request):
+        # Parameter existence check
+        if not 'id' in request.GET:
+            return Response(
+                    {'error': '"id" parameter is required.'},
+                    status=status.HTTP_400_BAD_REQUEST,
+                    )
+        # Serialize
+        serializer = UnivSerializer(Univ.objects.all(), many=True, context={'request': request})
+        return Response(serializer.data)
+
+    def post(self, request):
         # Parameter existence check
         required_parameters = ('id', 'univ', 'jh', 'major')
         for param in required_parameters:
@@ -26,7 +31,10 @@ class DeviceInfo(APIView):
                         )
         # Create device info
         values = {param: request.GET.get(param) for param in required_parameters}
-        device = Device.objects.create(unique_id=values.get('id'))
+        try:
+            device = Device.objects.create(unique_id=values.get('id'))
+        except IntegrityError:
+            return Response({'error': 'Device ID is duplicated.'})
         device.majors.add(
                 Major.objects.get(
                     Q(jh__sj__univ__name=values.get('univ')) &
@@ -34,10 +42,8 @@ class DeviceInfo(APIView):
                     Q(name=values.get('major'))
                     )
                 )
-        # Device duplication check & save
-        try:
-            device.save()
-        except IntegrityError:
-            return Response({'error': 'Device ID is duplicated.'})
-        return Response({'success': 'Successfully saved data.'})
+        device.save()
+        # Serialize
+        serializer = UnivSerializer(Univ.objects.all(), many=True, context={'request': request})
+        return Response(serializer.data)
 
