dafe47e00697f95141d808a7c48eb231d609e3a3
podobongbong <kim11560@snu.ac.kr>
2020-07-08 01:22:30 +0900

    good scheduler

--- scd.py
+++ scd.py
@@ -12,7 +12,6 @@ django.setup()
 from univ.models import Schedule
 from temp import send_fcm_notification
 
-
 KST = timezone('Asia/Seoul')
 
 
@@ -21,20 +20,18 @@ def notify():
     for schedule in schedules:
         liked_users = schedule.major.devices.all()
         tokens = [user.token for user in liked_users]
-        start_date = schedule.start_date
         day_before = timedelta(days=1)
         now_hour = KST.localize(datetime.now().replace(minute=0, second=0, microsecond=0))
-        if now_hour == (start_date - day_before):
-            print(tokens[0])
-            print(schedule.description)
-            send_fcm_notification(tokens[0], '제목', schedule.description)
+        if now_hour == (schedule.start_date - day_before):
+            for token in tokens:
+                send_fcm_notification(token, '제목', schedule.description)
 
 
 if __name__ == '__main__':
     scheduler = BackgroundScheduler()
     scheduler.configure(timezone=KST)
     scheduler.start()
-    scheduler.add_job(notify, 'interval', seconds=5)
+    scheduler.add_job(notify, 'interval', hours=1)
 
     while True:
         time.sleep(1)
