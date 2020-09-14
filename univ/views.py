ec77aa306991cd099d2f5b5d0ea53d68ba9e0729
podobong <kim11560@snu.ac.kr>
2020-09-14 10:32:07 +0000

    typo

--- univ/views.py
+++ univ/views.py
@@ -17,9 +17,36 @@ class UnivList(APIView):
         try:
             device = Device.objects.get(unique_id=request.GET.get('id'))
         except Device.DoesNotExist:
-            return Response(
-                    {'error': 'Device does not exist. To add a new device, use POST method.'},
-                    status=status.HTTP_400_BAD_REQUEST,
+            return Response([
+            {
+                "name": "공지사항",
+                "logo": "",
+                "review_url": "",
+                "sjs": [
+                    {
+                        "sj": "",
+                        "jhs": [
+                            {
+                                "name": "입시 일정 변동으로 인하여 데이터가 초기화 되었습니다.",
+                                "majors": [
+                                    {
+                                        "name": "",
+                                        "schedules": [
+                                            {
+                                                "is_valid": 1,
+                                                "description": "",
+                                                "start_date": "2020-09-14-00-00-00",
+                                                "end_date": "2020-09-14-00-00-00"
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }],
+            status=status.HTTP_400_BAD_REQUEST,
             )
         majors = device.majors.all()
         jhs = sorted(list(set([major.jh for major in majors])))
